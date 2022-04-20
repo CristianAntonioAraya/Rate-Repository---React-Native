ad6ef6f5df30e8591a94d4cd124d3c424a4f7f57
Dev Cristian Araya <dev.arayacristian@gmail.com>
2022-04-20 15:12:11 -0400

    Start Application

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+
+const Main = () => {
+    return (
+        <View>
+            <Text>Rate Repository Application</Text>
+        </View>
+    )
+}
+
+export default Main
